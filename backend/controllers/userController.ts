8698cff8db31318c6277012286c109050171a95a
Fabian Salge <fabian.salge.2020@uni.strath.ac.uk>
2023-01-30 01:00:22 +0000

    feat: add addToWaitlist, removeTopWaitlist and removeFromWaitlist endpoints

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -43,6 +43,47 @@ export class UserController {
 			return res.status(500).send({ message: "Internal server error" });
 		}
 	}
+
+	async addToWaitlist(req: Request, res: Response): Promise<Response> {
+		try {
+			const { email, fullname } = req.body;
+			const user = await userRepository.getUserByEmail(email);
+
+			if (user) {
+				return res.status(400).send({ message: "Already a user" });
+			}
+
+			await userRepository.addToWaitlist(email, fullname);
+			return res.status(200).send({ message: "Added to waitlist" });
+		} catch (error) {
+			console.error(error);
+			logger.error(`User: ${req.body.email} not added to waitlist!`);
+			return res.status(500).send({ message: "Internal server error" });
+		}
+	}
+
+	async removeTopWaitlist(_: Request, res: Response): Promise<Response> {
+		try {
+			await userRepository.takeOffWaitlist();
+			return res.status(200).send({ message: "Removed first person off waitlist" });
+		} catch (error) {
+			console.error(error);
+			logger.error("Not removed from waitlist.");
+			return res.status(500).send({ message: "Internal server error" });
+		}
+	}
+
+	async removeFromWaitlist(req: Request, res: Response): Promise<Response> {
+		try {
+			const email = req.body.email;
+			await userRepository.removeFromWaitlist(email);
+			return res.status(200).send({ message: `Removed ${email} off waitlist` });
+		} catch (error) {
+			console.error(error);
+			logger.error(`Did not remove ${req.body.email} from wailist`);
+			return res.status(500).send({ message: "Internal server error" });
+		}
+	}
 }
 
 export default UserController;
--- backend/repositories/userRepository.ts
+++ backend/repositories/userRepository.ts
@@ -1,5 +1,6 @@
 import bcrypt = require("bcrypt");
 import { PrismaClient, User } from "@prisma/client";
+import { Waitlist } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -25,5 +26,71 @@ export class UserRepository {
 			},
 		});
 		return user;
+	}
+
+	async takeOffWaitlist(): Promise<void> {
+		await prisma.waitlist.delete({
+			where: {
+				position: 1,
+			},
+		});
+
+		await prisma.waitlist.updateMany({
+			where: {
+				position: {
+					gt: 1,
+				},
+			},
+			data: {
+				position: {
+					decrement: 1,
+				},
+			},
+		});
+	}
+
+	async addToWaitlist(email: string, fullname: string): Promise<Waitlist> {
+		const highestPosition: number | null = await prisma.waitlist
+			.aggregate({ _max: { position: true } })
+			.then((result) => result._max.position);
+
+		return prisma.waitlist.create({
+			data: {
+				email,
+				fullname,
+				position: (highestPosition ? highestPosition : 0) + 1,
+			},
+		});
+	}
+
+	async removeFromWaitlist(email: string): Promise<void> {
+		const pos = await prisma.waitlist.findUnique({
+			where: {
+				email: email,
+			},
+		});
+
+		await prisma.waitlist.delete({
+			where: {
+				email: email,
+			},
+		});
+
+		if (!pos) {
+			return;
+		} else {
+			await prisma.waitlist.updateMany({
+				where: {
+					position: {
+						gt: pos?.position
+					}
+				},
+				data: {
+					position: {
+						decrement: 1,
+					},
+				},
+			});
+		}
 	}
 }
